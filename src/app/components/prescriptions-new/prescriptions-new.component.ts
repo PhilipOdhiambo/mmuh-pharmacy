1534e19eb610b6d79f21e7414fde7ecacc34d161
PhilipOdhiambo <jofonowino@gmail.com>
2023-12-25 07:25:11 +0300

    saved chages

--- src/app/components/prescriptions-new/prescriptions-new.component.ts
+++ src/app/components/prescriptions-new/prescriptions-new.component.ts
@@ -17,17 +17,14 @@ export class PrescriptionsNewComponent {
   osList = []
 
   cartegory = 'prescription'
-  date:any
-  billStart:any
+  billStart = new Date()
   billEnd:any
   dispenseStart:any
   dispenseEnd:any
   user:any
-  Id:any
   tallyNo:any
-  itemsOrdered = null
-  itemsAvailable:any
-  itemsSubstituted:any
+  itemsOrdered:any
+  itemsUsubstitutable = 0
 
   constructor(private appService: AppService){}
 
@@ -41,8 +38,9 @@ export class PrescriptionsNewComponent {
         Cartegory: this.cartegory,
         tallyNo: nextTally,
         itemsOrdered: this.itemsOrdered,
-        itemsAvailable: this.itemsAvailable,
-        billStart:this.billStart
+        itemsAvailable: this.itemsOrdered - this.itemsUsubstitutable,
+        billStart:this.billStart,
+        billEnd: new Date()
       }
     ]
     this.appService.doPost('outpatients','create',data)
