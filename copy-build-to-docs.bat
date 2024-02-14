0f38a36bb5b81ef74b7aaba7b3f6def3d85bb228
Philip Owino <jofonowino@gmail.com>
2024-02-14 16:29:03 +0300

    untracked copy paste

--- copy-build-to-docs.bat
+++
@@ -1,8 +0,0 @@
-@echo OFF
-SETLOCAL
-FOR  /d %%i IN (docs\*) DO RD "%%i" /S /Q
-FOR %%i IN (docs\*) DO IF /i NOT "%%~nxi"=="CNAME" IF /i NOT "%%~nxi"=="404.html" DEL "%%i"
-
-xcopy /E /I dist\browser\assets docs\assets
-copy  dist\browser  docs\
-copy dist\*.txt docs\
